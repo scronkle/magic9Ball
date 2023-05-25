aecf90f5739851fd6540fdf84a35844deefa0e18
scronkle <widdlyscudspizza@gmail.com>
2023-05-25 23:14:12 +1000

    add valence function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 let scriptExists = true
+
+let valence = 0
+let strength = 0
+let confidence = 0
+
+function selectValence() {
+  valence = Math.floor(Math.random()*3)
+  return valence
+}
+
+console.log(selectValence())
