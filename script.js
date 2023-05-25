b2cc662fafa3375f7d69bd8d7443cbe2d83d269f
scronkle <widdlyscudspizza@gmail.com>
2023-05-25 23:31:57 +1000

    add answer generator

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let valence = 0
 let strength = 0
 let confidence = 0
 
+const valenceArr = ["NO", "MAYBE", "YES"]
+const strengthArr = ["very weak", "weak", "moderate", "strong", "very strong"]
+
 
 //set valence to one of 3 values
 function selectValence() {
@@ -23,3 +26,13 @@ function selectConfidence() {
   confidence = Math.floor(Math.random()*100)
   return confidence
 }
+
+function generateAnswer() {
+  let userValence = valenceArr[selectValence()]
+  let userStrength = strengthArr[selectStrength()]
+  console.log(`The answer is a ${userStrength} ${userValence}.`)
+  selectConfidence()
+  console.log(`I am ${confidence}% confident about this.`)
+}
+
+generateAnswer()
