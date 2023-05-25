bd386b0863f2ec5b4094bdd4d71b2784c4ff2781
scronkle <widdlyscudspizza@gmail.com>
2023-05-25 23:22:09 +1000

    add strength and confidence selectors

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ let valence = 0
 let strength = 0
 let confidence = 0
 
+
+//set valence to one of 3 values
 function selectValence() {
   valence = Math.floor(Math.random()*3)
   return valence
 }
 
-console.log(selectValence())
+
+//set strength to one of 5 values
+function selectStrength() {
+  strength = Math.floor(Math.random()*5)
+  return strength
+}
+
+//set confidence to a percentage?
+function selectConfidence() {
+  confidence = Math.floor(Math.random()*100)
+  return confidence
+}
